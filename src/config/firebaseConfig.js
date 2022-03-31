be71d5d1a5f999157452051362fcc0e7f3b65c9e
Franco Valledor <franco.valledor@tailorsoft.co>
2022-03-31 01:05:18 -0300

    firebase config added

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAulyJ471TK-n7vIlNkdcyw4FgT7lYOJ9A',
+  authDomain: 'ejemplo-50c47.firebaseapp.com',
+  projectId: 'ejemplo-50c47',
+  storageBucket: 'ejemplo-50c47.appspot.com',
+  messagingSenderId: '309331643147',
+  appId: '1:309331643147:web:40e5136944b02e7251700e',
+  measurementId: 'G-8QJWTBY6Q2',
+};
+
+export default firebaseConfig;
